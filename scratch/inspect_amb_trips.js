daea0adfa06b8aa7b29d69c1bffbc9f84ac48c8d
Some1sm <ceperodavid@gmail.com>
2026-08-17 23:29:49 +0200

    chore: clean up legacy static files, scratch scripts, and unused AMB GTFS dump

--- scratch/inspect_amb_trips.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const trips = fs.readFileSync('data/amb_gtfs/trips.txt', 'utf8').split('\n').filter(Boolean);
-console.log('Total trips:', trips.length);
-console.log('Sample trip header & row:', trips.slice(0, 3));
-
-const stopTimes = fs.readFileSync('data/amb_gtfs/stop_times.txt', 'utf8').split('\n').filter(Boolean);
-console.log('Total stop times:', stopTimes.length);
-console.log('Sample stop time header & row:', stopTimes.slice(0, 3));
